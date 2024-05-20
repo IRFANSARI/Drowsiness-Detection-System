4e09b7de885932d4b42153b2931e8076d2bceb21
Irfan Ansari <ia4894799@gmail.com>
2024-05-20 13:40:40 +0530

    install_dependecies.bat file added

---
+++ install_dependencies.bat
@@ -0,0 +1,9 @@
+pip install whl
+pip install opencv-python
+pip install imutils
+
+curl https://raw.githubusercontent.com/IRFANSARI/DLib-for-Python-3.11/main/dlib-19.24.1-cp311-cp311-win_amd64.whl --output dlib-19.24.1-cp311-cp311-win_amd64.whl
+pip install dlib-19.24.1-cp311-cp311-win_amd64.whl
+del dlib-19.24.1-cp311-cp311-win_amd64.whl
+
+curl https://raw.githubusercontent.com/italojs/facial-landmarks-recognition/master/shape_predictor_68_face_landmarks.dat --output shape_predictor_68_face_landmarks.dat
